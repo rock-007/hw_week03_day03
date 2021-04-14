4ce4fc109a443b5ac8fb1f42e15a2ff64b3cc74b
rock-007 <umair.ashraf2019@gmail.com>
2021-04-14 19:24:04 +0100

    first test passed

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,13 @@
 import unittest
-from controllers.controller import *
+# from controllers.controller import *
+from modules.calculator import Calculator
 
 class Test_calculator(unittest.TestCase):
 
     def setUp(self):
         self.number_1 = 3
         self.number_2 = 4
+        self.calculator = Calculator("normal_calculator")
 
-    def test_calulator_adding_two_values(self.number_1,self.number_2):
-        self.assertEqual(7,)
+    def test_calulator_adding_two_values(self):
+        self.assertEqual(7,self.calculator.add_two_numbers(self.number_1, self.number_2))
