f23bc3c31055eb08591178edb807a926ca3a2fb8
rock-007 <umair.ashraf2019@gmail.com>
2021-04-14 19:27:10 +0100

    all four test passed

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,7 +7,17 @@ class Test_calculator(unittest.TestCase):
     def setUp(self):
         self.number_1 = 3
         self.number_2 = 4
+        self.number_3 = 8
         self.calculator = Calculator("normal_calculator")
 
     def test_calulator_adding_two_values(self):
         self.assertEqual(7,self.calculator.add_two_numbers(self.number_1, self.number_2))
+
+    def test_calulator_multiplying_two_values(self):
+        self.assertEqual(12,self.calculator.multiply_two_numbers(self.number_1, self.number_2))
+
+    def test_calulator_substraction_two_values(self):
+        self.assertEqual(5,self.calculator.subtract_two_numbers(self.number_3, self.number_1))
+
+    def test_calulator_division_two_values(self):
+        self.assertEqual(2,self.calculator.divide_two_numbers(self.number_3, self.number_2))
